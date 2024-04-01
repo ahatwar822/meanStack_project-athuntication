751d485a77265d29ea58c5bcf13168fe68c7dc81
himanshugaupale <himanshugaupale875@gmail.com>
2024-04-01 16:57:01 +0530

    commited1

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,12 @@
 import Role from "../models/Role.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import Jwt from "jsonwebtoken";
+import { CreateSuccess } from "../utils/success.js";
 
 
 export const register = async (req, res, next) => {
+    
     const role = await Role.find ({ role : "User" });
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(req.body.password, salt);
@@ -17,12 +20,15 @@ export const register = async (req, res, next) => {
         role: role
     });
     await newUser.save();
-    res.send ("user created");
+    return next(CreateSuccess(200, "User created successfully", newUser));
 }
 
 export const login = async (req, res, next) => {  
     try{
-        const user = await User.findOne({email: req.body.email});
+        const user = await User.findOne({email: req.body.email})
+        .populate("roles","role");
+
+        const { roles } = user;
         if (!user) {
             return res.status(404).send("User not found")
         }
@@ -30,7 +36,18 @@ export const login = async (req, res, next) => {
         if (!isPasswordCorrect) {
             return res.status(400).send('password is not correct');
         }
-        return res.status(200).send("LAgin successful");
+        const token = Jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin, roles:roles},
+            process.env.JWT_SECRET
+            )
+            res.cookie("access_token", token, {httpOnly: true})
+            .status(200)
+            .json({
+                status: 200,
+                message: "User logged in successfully",
+                data: user
+            });
+        
     }catch (error){
         return res.status(500).send("something went wrong");
     }
--- api/index.js
+++ api/index.js
@@ -11,6 +11,18 @@ app.use (express.json());
 app.use('/api/role', roleRoute);
 app.use('/api/auth', authRoute);
 
+// Response handler middleware
+app.use ((obj, req, res, next) => {
+    const statusCode = obj.status || 500;
+    const message = obj.message || "Something went wrong!";
+    return res.status(statusCode).json({
+        success: [200,201,204].some(a => a === obj.status)? true : false,
+        status: statusCode,
+        message: message,
+        stack: obj.data
+    });
+});
+
 // app.use('/', (req,res) => {
 //     return res.send ("<h1> Welcome to the meanStak </h1>")
 // });
